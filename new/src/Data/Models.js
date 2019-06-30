edadf88f8cd721154947eb8c1c01ad616603198e
Oliinuk Olexandr <olexandr248@gmail.com>
2019-07-01 00:47:53 +0300

    deleted link

--- new/src/Data/Models.js
+++ new/src/Data/Models.js
@@ -1,18 +1,13 @@
 const model = [
     {
         id:1,
-        href:'https://github.com/Teorares/model1',
+        href:'https://github.com/Teorares/model2',
         text: 'First model',
     },
     {
         id:2,
-        href:'https://github.com/Teorares/model2',
-        text: 'Second model',
-    },
-    {
-        id:3,
         href:'https://github.com/Teorares/model3',
-        text: 'Third model',
+        text: 'Second model',
     },
 ]
 
